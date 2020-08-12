98fc7e4f787ae9c9fad66c5eba3c84c421baf282
Dora Belme <contact.dorabelme@gmail.com>
2020-08-11 21:25:42 -0400

    form validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -17,32 +17,78 @@ export class Register extends Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: [],
+            loading: false
         };
-    }
+    };
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if (this.isFormEmpty(this.state)) {
+            error = { message: 'Fill in all fields' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.isPasswordValid(this.state)) {
+            error = { message: 'Password is invalid' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length
+    };
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>)
 
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
-    }
+    };
 
-    handleSubmit = (event) => {
+    handleSubmit = event => {
+        event.preventDefault();
         if (this.isFormValid()) {
-            event.preventDefault();
+            this.setState({ errors: [], loading: true });
             firebase
                 .auth()
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser);
+                    this.setState({ loading: false });
                 })
                 .catch(err => {
-                    console.log(err);
-                })
+                    console.error(err);
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                        loading: false
+                    });
+                });
         }
+    };
 
-    }
+    handleInputError = (errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName))
+            ? "error"
+            : "";
+    };
 
     render() {
-        const { username, email, password, passwordConfirmation } = this.state;
+        const { username, email, password, passwordConfirmation, errors, loading } = this.state;
         return (
             <Grid textAlign="center" verticalAlign="middle" className='app'>
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -69,6 +115,7 @@ export class Register extends Component {
                                 iconPosition="left"
                                 placeholder="Email Addresss"
                                 value={email}
+                                className={this.handleInputError(errors, 'email')}
                                 onChange={this.handleChange}
                                 type="email"
                             />
@@ -79,6 +126,8 @@ export class Register extends Component {
                                 iconPosition="left"
                                 placeholder="Password"
                                 value={password}
+                                className={this.handleInputError(errors, 'password')}
+
                                 onChange={this.handleChange}
                                 type="password"
                             />
@@ -88,13 +137,20 @@ export class Register extends Component {
                                 icon="repeat"
                                 iconPosition="left"
                                 placeholder="Password Confirmation"
+                                className={this.handleInputError(errors, 'password')}
+
                                 value={passwordConfirmation}
                                 onChange={this.handleChange}
                                 type="password"
                             />
-                            <Button fluid size="large" color="red"></Button>
+                            <Button disabled={loading} className={loading ? 'loading' : ''} fluid size="large" color="red">Submit</Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>)}
                     <Message>Already a User? <Link to='/login'>Login</Link></Message>
                 </Grid.Column>
             </Grid>
